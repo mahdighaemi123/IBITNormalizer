86d6fa88538f68813031861a80d393de830f4c0b
“Mahdi Ghaemi” <mahdighaemi123@gmail.com>
2024-03-08 01:25:10 +0330

    update tests

--- tests/test_normalizer.py
+++ tests/test_normalizer.py
@@ -5,7 +5,8 @@ from IBITNormalizer.normalizer import IBITNormalizer
 class TestNormalizer(unittest.TestCase):
 
     def test_normalize_text(self):
-        self.assertEqual(IBITNormalizer.forLm.normalize('سلام'), 'سلام')
+        self.assertEqual(IBITNormalizer.forLM().normalize('سلام'), 'سلام')
+        self.assertEqual(IBITNormalizer.forLLM().normalize('سلام'), 'سلام')
 
 
 if __name__ == '__main__':
